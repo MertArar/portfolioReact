fa00538cbbcf60e4f60a8afc6865cb14248d946c
MertArar <85193329+MertArar@users.noreply.github.com>
2023-05-03 03:34:31 +0300

    banner done

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,7 +1,17 @@
 import React from "react";
+import LeftBanner from "./LeftBanner";
+import RightBanner from "./RightBanner";
 
 const Banner = () => {
-  return <div>banner</div>;
+  return (
+    <section
+      id="home"
+      className="w-full pt-10 pb-20 flex flex-col gap-10 xl:gap-0 lgl:flex-col items-center border-b-[1px] font-titleFont border-b-black"
+    >
+      <LeftBanner />
+      <RightBanner />
+    </section>
+  );
 };
 
 export default Banner;
