1aac071faea673fd2504f7efee904e60dfc89a2d
MertArar <85193329+MertArar@users.noreply.github.com>
2023-05-04 05:03:34 +0300

    Education of resume added

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import Title from "../layouts/Title";
 
 const Resume = () => {
-  return <div>Resume</div>;
+  const [educationData, setEducationData] = useState(true);
+  const [skillData, setSkillData] = useState(false);
+  const [experienceData, setExperienceData] = useState(false);
+  const [achievementData, setAchievementData] = useState(false);
+  return (
+    <section>
+      <div>
+        <Title title="7+ YEARS OF EXPERIENCE" des="My Resume" />
+      </div>
+      <div>
+        <ul>
+          <li
+            onClick={() =>
+              setEducationData(true) &
+              setSkillData(false) &
+              setExperienceData(false) &
+              setAchievementData(false)
+            }
+            className={`${
+              educationData
+                ? "border-designColor rounded-lg"
+                : "border-transparent"
+            }resumeLi `}
+          >
+            Education
+          </li>
+          <li
+            onClick={() =>
+              setEducationData(false) &
+              setSkillData(true) &
+              setExperienceData(false) &
+              setAchievementData(false)
+            }
+            className={`${
+              skillData ? "border-designColor rounded-lg" : "border-transparent"
+            }resumeLi `}
+          >
+            Professional Skills
+          </li>
+          <li
+            onClick={() =>
+              setEducationData(false) &
+              setSkillData(false) &
+              setExperienceData(true) &
+              setAchievementData(false)
+            }
+            className={`${
+              experienceData
+                ? "border-designColor rounded-lg"
+                : "border-transparent"
+            }resumeLi `}
+          >
+            Experience
+          </li>
+          <li
+            onClick={() =>
+              setEducationData(false) &
+              setSkillData(false) &
+              setExperienceData(false) &
+              setAchievementData(true)
+            }
+            className={`${
+              achievementData
+                ? "border-designColor rounded-lg"
+                : "border-transparent"
+            }resumeLi `}
+          >
+            Achievement
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Resume;
