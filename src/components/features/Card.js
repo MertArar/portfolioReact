31aa57cd2bb491aef091ef9303d27fcf486f560f
MertArar <mertarar.ma@gmail.com>
2023-05-11 04:50:31 +0300

    motion added

--- src/components/features/Card.js
+++ src/components/features/Card.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { HiArrowRight } from "react-icons/hi";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variant";
 
 const Card = ({ item: { title, des, icon } }) => {
   return (
-    <div className="w-full px-12 h-80 py-10 rounded-lg shadow-shadowOne flex items-center bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-black hover:to-[#1e2024] transition-colors duration-100 group">
+    <motion.div
+      variants={fadeIn("up", 0.4)}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: false, amount: 0.7 }}
+      className="w-full px-12 h-80 py-10 rounded-lg shadow-shadowOne flex items-center bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-black hover:to-[#1e2024] transition-colors duration-100 group"
+    >
       <div className="h-72 overflow-y-hidden">
         <div className="flex h-full flex-col gap-10 translate-y-16 group-hover:translate-y-0 transition-transform duration-500">
           <div className="w-10 h-8 flex flex-col justify-between">
@@ -29,7 +37,7 @@ const Card = ({ item: { title, des, icon } }) => {
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -2,14 +2,22 @@ import React from "react";
 import Title from "../layouts/Title";
 import Card from "./Card";
 import { featuresData } from "../../data/data";
-
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variant";
 const Features = () => {
   return (
     <section
       id="features"
       className="w-full py-20 border-b-[1px] border-b-black"
     >
-      <Title title="Features" desc="What I Do" />
+      <motion.div
+        variants={fadeIn("right", 0.4)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.7 }}
+      >
+        <Title title="Features" desc="What I Do" />
+      </motion.div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
         {featuresData.map((item) => (
           <Card item={item} key={item.id} />
